143a7f9abbfc1bad94c61b9beb904e07c2b43fe0
Emil Tullstedt <sakjur@gmail.com>
2012-04-14 15:46:07 +0200

    Added challenge 14

--- Challenges/challenge14/main.js
+++ Challenges/challenge14/main.js
@@ -1,3 +1,22 @@
 i = 15432;
 
-i.split("");
+function whatNums(i) {
+	s = i.toString();
+	s = s.split("");
+	s = s.sort();
+	s = s.join();
+	return s;
+}
+
+ifind = i;
+
+found = false;
+
+while(!found)
+{	
+	if(ifind > i && whatNums(ifind) == whatNums(i)){
+		console.log(ifind);
+		found = true;
+	}
+	ifind++;
+}
